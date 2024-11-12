d5d437ecad7b107c57041fe2f540d454d05ec2e9
Alan Emiliano Alcaraz Rocha <alcarazemi2003@gmail.com>
2024-11-11 19:29:53 -0600

    commit pizzeria 11 nov

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CapturaPedidoComponent } from './pizzas/captura-pedido/captura-pedido.component';
+import { DetallePedidoComponent } from "./pizzas/detalle-pedido/detalle-pedido.component";
+import { VentasDiaComponent } from "./pizzas/ventas-dia/ventas-dia.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DetallePedidoComponent, VentasDiaComponent, CapturaPedidoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
